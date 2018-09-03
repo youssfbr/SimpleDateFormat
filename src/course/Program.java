77a4833a8953ec94ebe49a6062ebe8de297c3490
Alisson Youssf <youssfbr@gmail.com>
2018-09-03 17:36:34 -0300

    Outra forma de pegar a data com instant atual

--- src/course/Program.java
+++ src/course/Program.java
@@ -16,11 +16,13 @@ public class Program {
 		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
 		Date x1 = new Date();
+		Date x2 = new Date(System.currentTimeMillis());
 		
 		Date y1 = sdf1.parse("03/09/2018"); 
 		Date y2 = sdf2.parse("03/09/2018 17:20:07");
 		
 		System.out.println("x1: " + sdf2.format(x1));
+		System.out.println("x2: " + sdf2.format(x2));
 		System.out.println("y1: " + sdf1.format(y1));
 		System.out.println("y2: " + sdf2.format(y2));
 	}
