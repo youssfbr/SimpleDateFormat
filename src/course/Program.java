10c20c11015749ceb470add4c370cdb9545e24d9
Alisson Youssf <youssfbr@gmail.com>
2018-09-03 17:29:20 -0300

    Instanciando, formatando e mostrando na tela

--- src/course/Program.java
+++ src/course/Program.java
@@ -1,12 +1,25 @@
 package course;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /*
  * https://docs.oracle.com/javase/10/docs/api/java/util/Date.html
  * Alisson Youssf - Revisão emm 03/09/2018. 
  */
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-	}
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Date y1 = sdf1.parse("03/09/2018"); 
+		Date y2 = sdf2.parse("03/09/2018 17:20:07");
+		
+		System.out.println("y1: " + sdf1.format(y1));
+		System.out.println("y2: " + sdf2.format(y2));
 
+	}
 }
