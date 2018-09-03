82a83a0dd52e31cf3296744b636f2e1ecefe657b
Alisson Youssf <youssfbr@gmail.com>
2018-09-03 17:33:09 -0300

    Date com instant atual

--- src/course/Program.java
+++ src/course/Program.java
@@ -15,11 +15,13 @@ public class Program {
 		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
 		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
+		Date x1 = new Date();
+		
 		Date y1 = sdf1.parse("03/09/2018"); 
 		Date y2 = sdf2.parse("03/09/2018 17:20:07");
 		
+		System.out.println("x1: " + sdf2.format(x1));
 		System.out.println("y1: " + sdf1.format(y1));
 		System.out.println("y2: " + sdf2.format(y2));
-
 	}
 }
