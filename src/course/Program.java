dacbbe4da5ba682ae72d2947ffc59a06053afcb9
Alisson Youssf <youssfbr@gmail.com>
2018-09-04 09:20:12 -0300

    Date 0 ms

--- src/course/Program.java
+++ src/course/Program.java
@@ -19,12 +19,14 @@ public class Program {
 		
 		Date x1 = new Date();
 		Date x2 = new Date(System.currentTimeMillis());
+		Date x3 = new Date(0);
 		
 		Date y1 = sdf1.parse("03/09/2018"); 
 		Date y2 = sdf2.parse("03/09/2018 17:20:07");
 		
 		System.out.println("x1: " + sdf2.format(x1));
 		System.out.println("x2: " + sdf2.format(x2));
+		System.out.println("x3: " + sdf2.format(x3));
 		System.out.println("y1: " + sdf1.format(y1));
 		System.out.println("y2: " + sdf2.format(y2));
 		//
