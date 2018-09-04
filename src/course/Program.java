7ad8363155d94cf44de506fc0c723ae9700db328
Alisson Youssf <youssfbr@gmail.com>
2018-09-04 09:29:05 -0300

    Date 5 ms

--- src/course/Program.java
+++ src/course/Program.java
@@ -20,6 +20,7 @@ public class Program {
 		Date x1 = new Date();
 		Date x2 = new Date(System.currentTimeMillis());
 		Date x3 = new Date(0);
+		Date x4 = new Date(1000L * 60L * 60L * 5L);
 		
 		Date y1 = sdf1.parse("03/09/2018"); 
 		Date y2 = sdf2.parse("03/09/2018 17:20:07");
@@ -27,8 +28,8 @@ public class Program {
 		System.out.println("x1: " + sdf2.format(x1));
 		System.out.println("x2: " + sdf2.format(x2));
 		System.out.println("x3: " + sdf2.format(x3));
+		System.out.println("x4: " + sdf2.format(x4));
 		System.out.println("y1: " + sdf1.format(y1));
 		System.out.println("y2: " + sdf2.format(y2));
-		//
 	}
 }
